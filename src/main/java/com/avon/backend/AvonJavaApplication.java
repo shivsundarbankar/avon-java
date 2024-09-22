a05dac6d9303aa8ae65617a1ad30a76153f1fea5
shivsundarbankar <shivsundarbankar@gmail.com>
2024-09-22 17:21:29 +0530

    Starter pack - logger, checkstyle, profiles

---
+++ src/main/java/com/avon/backend/AvonJavaApplication.java
@@ -0,0 +1,11 @@
+package com.avon.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvonJavaApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AvonJavaApplication.class, args);
+  }
+}
---
+++ src/test/java/com/avon/backend/AvonJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avon.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvonJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
